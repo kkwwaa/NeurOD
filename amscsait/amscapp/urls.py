44f204c2ce81d1176cadb46342f9cd771eee9e5d
Kate Basova <120415812+kkwwaa@users.noreply.github.com>
2023-08-28 11:30:43 +0300

    Update urls.py

--- amscsait/amscapp/urls.py
+++ amscsait/amscapp/urls.py
@@ -1,6 +1,7 @@
 from amscsait import settings
 from . import views
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
