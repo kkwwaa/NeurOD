fedd32382fe97b09b175ef7180ecb11ca4e016f3
Kate Basova <120415812+kkwwaa@users.noreply.github.com>
2023-09-08 00:38:27 +0300

    Update urls.py

--- amscsait/amscapp/urls.py
+++ amscsait/amscapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("list", views.index, name="index"),
     path("admin/", views.admin, name="admin"),
     path("create_proba", views.create_proba, name="create_proba"),
     path("create_patient", views.create_patient, name="create_patient"),
@@ -14,7 +14,8 @@ urlpatterns = [
     path("patient/<int:pk>/probs_list/<int:proba_pk>", views.proba, name="proba"),
     path("patient/<int:pk>/probs_results/<str:slug>", views.probs_results, name="probs_results"),
     path("register", views.register, name="register"),
-    path("login", views.user_login, name="login"),
+    #path("login", views.user_login, name="login"),
+    path("", views.user_login, name='login'),
     path("exit", views.exit, name="exit"),
 ]
 
